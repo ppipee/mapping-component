c9dbb01563a517b8c586cd526b60b18f74d3f378
kittapon.j <kittapon.j@lmwn.com>
2022-11-21 16:24:43 +0700

    init mapping-component

---
+++ src/Component.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getContext } from 'svelte'
+
+  export let dataSource = []
+
+  const { styleable, Provider } = getContext('sdk')
+  const component = getContext('component')
+</script>
+
+<div use:styleable={$component.styles}>
+  {#each dataSource as data, index}
+    <Provider data={{ data, index, length: dataSource.length }}>
+      <slot />
+    </Provider>
+  {/each}
+</div>

c9a7e52b7089e43d2757a530a0ac0d95121f4a31
kittapon.j <kittapon.j@lmwn.com>
2022-11-23 11:09:40 +0700

    fix: handle data source error

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,5 +1,7 @@
 <script>
   import { getContext } from 'svelte'
+  import isArray from 'lodash/isArray'
+  import isEmpty from 'lodash/isEmpty'
 
   export let dataSource = []
 
@@ -8,9 +10,15 @@
 </script>
 
 <div use:styleable={$component.styles}>
-  {#each dataSource as data, index}
-    <Provider data={{ data, index, length: dataSource.length }}>
-      <slot />
-    </Provider>
-  {/each}
+  {#if isEmpty(dataSource)}
+    a data source must have
+  {:else if !isArray(dataSource)}
+    a data source must be array
+  {:else}
+    {#each dataSource as data, index}
+      <Provider data={{ data, index, length: dataSource.length }}>
+        <slot />
+      </Provider>
+    {/each}
+  {/if}
 </div>
